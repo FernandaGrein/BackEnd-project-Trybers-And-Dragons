42118963a4bebc0cfbe6c3e479e0e1b36e7afb55
Fernanda Grein <fernandagrein.fgm@outlook.com>
2022-10-19 17:48:54 -0300

    refatora requisito 01

--- src/Battle/Battle.ts
+++ src/Battle/Battle.ts
@@ -2,7 +2,6 @@ import Fighter from '../Fighter';
 
 abstract class Battle {
   constructor(protected player: Fighter) { }
-  //
   
   // Should return 1 if player wins, -1 otherwise
   fight(): number {
--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -1,8 +1,19 @@
 export default abstract class Race {
-  constructor(
-    public readonly name: string, 
-    public readonly dexterity: number,
-  ) { }
+  private readonly _name: string;
+  private readonly _dexterity: number;
+  
+  constructor(name: string, dexterity: number) {
+    this._name = name;
+    this._dexterity = dexterity;
+  }
+  
+  get name(): string {
+    return this._name;
+  }
+
+  get dexterity(): number {
+    return this._dexterity;
+  }
 
   static createdRacesInstances(): number {
     throw new Error('Not implemented');
