33d8168d52594dc969cdb9f963add0c0cf2b4c67
Fernanda Grein <fernandagrein.fgm@outlook.com>
2022-10-19 13:51:45 -0300

    erro de bug

--- src/Battle/Battle.ts
+++ src/Battle/Battle.ts
@@ -2,6 +2,7 @@ import Fighter from '../Fighter';
 
 abstract class Battle {
   constructor(protected player: Fighter) { }
+  //
   
   // Should return 1 if player wins, -1 otherwise
   fight(): number {
